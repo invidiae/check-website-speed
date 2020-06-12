916f73cf18eaca07cac166420b11e8ebf82f7fe7
Camillo <camilloblomberg@gmail.com>
2020-06-12 14:47:55 +0200

    ran the script for 5 min

--- V1.py
+++ V1.py
@@ -4,23 +4,22 @@ import random
 driver = webdriver.Chrome("/home/camillo/chromedriver")
 URL = "https://frontend-git-resolve-262.serlo.now.sh"
 driver.get(URL)
-end_time = time.time()+60*0.15
+end_time = time.time()+60*5
 links = []
 load = {}
+fächer = ["/mathe", "/informatik", "/biologie", "/nachhaltigkeit"]
 while end_time > time.time():
     wrong_link = False
     elems = driver.find_elements_by_xpath("//a[@href]")
     for elem in elems:
-        links.append(elem.get_attribute("href")) # add some randomness (maybe 10% of sample)
+        links.append(elem.get_attribute("href"))  # add some randomness (maybe 10% of sample)
     while not wrong_link:
         link = links[-1]
-        if link in load or not link[0:len(URL)+6] == URL+"/mathe":
-            print("falsch"+link)
+        if link in load or not link[0:len(URL)+len(fächer[0])] == URL+fächer[0]:
             del links[-1]
         else:
             wrong_link = True
             link = links.pop(-1)
-            print("richtig"+link)
     tic = time.time()
     driver.get(link)
     toc = time.time()
@@ -31,9 +30,10 @@ slow = {}
 for t in load:
     if load[t] > 2*avgload:
         slow[t] = load[t]
-with open ("ergebnis.txt", "w+") as results:
+
+with open("ergebnis.txt", "w+") as results:
     results.write("average loading speed in seconds: \n")
     results.write(str(avgload))
+    results.write("\n")
     results.write("sites with 100% more loading speed than average: \n")
     results.write(str(slow))
-
