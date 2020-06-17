cf9522ef84f48d985ad2abc5830ce0a5cedeb87f
Camillo <camilloblomberg@gmail.com>
2020-06-17 10:43:06 +0200

    V3.py geschrieben und ausgeführt, Plot gemacht usw

---
+++ README.md
@@ -0,0 +1,15 @@
+## create CSV file with timings of
+
+- **A:** frontend-git-resolve-262.serlo.now.sh/URL
+ 
+- **B:** frontend-git-resolve-262.serlo.now.sh/api/frontend/URL ohne cache
+ 
+- **C:** frontend-git-resolve-262.serlo.now.sh/api/frontend/URL mit cache
+ 
+- **C:** de.serlo-staging.dev/URL
+ 
+- **E:** de.serlo-staging.dev/api/frontend/URL
+
+
+## der Plot wird noch besser 
+(war der erste Versuch)
--- V3.py
+++ V3.py
@@ -1,56 +1,59 @@
 import csv
 import time
 import requests
-URLs = ["/115915", "/16043", "/27023", ""]
+URLs = ["/1595", "/74829", "/16043", "/75678", "/40786"]
 cookie = {'useFrontend':'0'}
 wo_cache = {'Authorization':'0'}
-anmeldung = {'Authorization': 'Basic c2VybG90ZWFtOnNlcmxvdGVhbQ=='}
-csv_header = ["URL", "Loading_Time", "Cache"]
+anmeldung = {'Authorization': 'Basic c3VybG90ZWFtOnNlcmxvdGVhbQ=='}
+csv_header = ["URL", "Loading_Time", "Cache", "Mode"]
 def A(URL):
-    FullURL = "http://frontend-git-resolve-262.serlo.now.sh"+URL
-    for i in range(7):
+    FullURL = "https://frontend-git-resolve-262.serlo.now.sh"+URL
+    for i in range(10):
         tic = time.time()
         r = requests.get(FullURL, cookies=cookie)
         toc = time.time()
-        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache']])
+        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "A"])
 def B(URL):
-    FullURL = "http://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
-    for i in range(7):
+    FullURL = "https://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
+    for i in range(10):
         tic = time.time()
         r = requests.get(FullURL, cookies=cookie, headers=wo_cache)
         toc = time.time()
-        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache']])
+        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "B"])
 def C(URL):
-    FullURL = "http://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
-    for i in range(7):
+    FullURL = "https://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
+    for i in range(10):
         tic = time.time()
         r = requests.get(FullURL, cookies=cookie)
         toc = time.time()
-        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache']])
+        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "C"])
 
 
 def D(URL):
-    FullURL = "http://de.serlo-staging.dev"+URL
-    for i in range(7):
+    FullURL = "https://de.serlo-staging.dev"+URL
+    for i in range(10):
         tic = time.time()
         r = requests.get(FullURL,cookies=cookie, headers=anmeldung)
         toc = time.time()
-        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache']])
+        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "D"])
 def E(URL):
-    FullURL = "http://de.serlo-staging.dev/api/frontend"+URL
-    for i in range(7):
+    FullURL = "https://de.serlo-staging.dev/api/frontend"+URL
+    for i in range(3):
         tic = time.time()
         r = requests.get(FullURL, cookies=cookie, headers=anmeldung)
         toc = time.time()
-        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache']])
+        writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "E"])
 
-with open("loadtimes.csv", "a", newline='') as loadtimes:
+with open("loadtimes.csv", "w", newline='') as loadtimes:
     writer = csv.writer(loadtimes, delimiter=',')
-  #  writer.writerow(csv_header)
+    writer.writerow(csv_header)
     for URL in URLs:
         A(URL)
+        print("1/5 done")
         B(URL)
+        print("2/5 done")
         C(URL)
-        B(URL)
+        print("3/5 done")
         D(URL)
+        print("4/5 done")
         E(URL)
