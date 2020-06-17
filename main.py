0533d3742ca05456121e5debe645d3837a36c7de
Camillo Blomberg <60753648+invidiae@users.noreply.github.com>
2020-06-17 11:01:49 +0200

    prettier lines

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ cookie = {'useFrontend':'0'}
 wo_cache = {'Authorization':'0'}
 anmeldung = {'Authorization': 'Basic c3VybG90ZWFtOnNlcmxvdGVhbQ=='}
 csv_header = ["URL", "Loading_Time", "Cache", "Mode"]
+
+
 def A(URL):
     FullURL = "https://frontend-git-resolve-262.serlo.now.sh"+URL
     for i in range(10):
@@ -13,6 +15,7 @@ def A(URL):
         r = requests.get(FullURL, cookies=cookie)
         toc = time.time()
         writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "A"])
+        
 def B(URL):
     FullURL = "https://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
     for i in range(10):
@@ -20,6 +23,7 @@ def B(URL):
         r = requests.get(FullURL, cookies=cookie, headers=wo_cache)
         toc = time.time()
         writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "B"])
+        
 def C(URL):
     FullURL = "https://frontend-git-resolve-262.serlo.now.sh/api/frontend"+URL
     for i in range(10):
@@ -28,7 +32,6 @@ def C(URL):
         toc = time.time()
         writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "C"])
 
-
 def D(URL):
     FullURL = "https://de.serlo-staging.dev"+URL
     for i in range(10):
@@ -36,6 +39,7 @@ def D(URL):
         r = requests.get(FullURL,cookies=cookie, headers=anmeldung)
         toc = time.time()
         writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "D"])
+        
 def E(URL):
     FullURL = "https://de.serlo-staging.dev/api/frontend"+URL
     for i in range(3):
@@ -44,6 +48,7 @@ def E(URL):
         toc = time.time()
         writer.writerow([FullURL, toc-tic, r.headers['x-vercel-cache'], "E"])
 
+        
 with open("loadtimes.csv", "w", newline='') as loadtimes:
     writer = csv.writer(loadtimes, delimiter=',')
     writer.writerow(csv_header)
